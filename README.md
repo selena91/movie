b33c86470994a7b5c43e56b2c1a68089cb0f4889
selena91 <selenamatijevic2@gmail.com>
2015-09-22 23:03:45 +0200

    algorithm finished
    Recommendation algorithm based on calculating Jaccard index is
    implemented

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # movie
 
-FIXME
+Project is developed in the LightTable IDE and Clojure programming language.
+NetFlix Prize dataset is used to test and implement algorithm for movies recommendation.
+
+### CONTENT
+Dataset is customized and data is splited to 3 tables:
+table customers [CustomerID, Password] - used for customer login to application
+table movies [MovieID, Title, YearOfRelease] - collection of all movies
+table ratings [MovieID, CustomerID, Rating, Date] - contains rates that customers gave to certain movies
+All tables' columns are strings, except column Rating which is a number between 1 and 5, and Date which is date.
+
+The purpose is to recommend to logged customer ten new movies (for example to watch and rate) based on his past rates.
+Firstly, similarity with other customers is determined by calculation of Jaccard similarity coefficient (https://en.wikipedia.org/wiki/Jaccard_index).
+Secondly, top ten movies rated with the highest rates by ten most similar customers are those recommended ones.
+
+The database used in project is SQLLite database located in file movie.rar.
 
 ## Prerequisites
 
--- src/movie/db.clj
+++ src/movie/db.clj
@@ -1,6 +1,7 @@
 (ns movie.db
 (:require [clojure.java.jdbc :as j]
-          [hiccup.page :as hiccP]))
+          [hiccup.page :as hiccP]
+          [clojure.string :as string]))
 
 (def db
   {:classname   "org.sqlite.JDBC"
@@ -13,7 +14,7 @@
     (j/query db ["select * from movies"]))
 
 (defn top10-movies
-  "List of top 10 movies"
+  "List of top 10 movies - recommended movies for customers who didn't give any rate"
   []
   (j/query db ["select * from movies
                  where MovieID in (
@@ -24,7 +25,7 @@
                 )"]))
 
 (defn do-login
-  "Check if customer exist in table customers"
+  "Check if customer exists in the table customers"
   [CustomerID Password]
     (j/query db
       ["select count(CustomerID) as total
@@ -40,7 +41,7 @@
 
 
 (defn count-customer-rating
-  "Check if customer have any rating"
+  "Check if customer gave any rate"
   [CustomerID]
     (j/query db
       ["select count(CustomerID) as total
@@ -58,60 +59,110 @@
       ))
 
 (defn all-customers
-  "Function which returns all customer ID"
+  "Function which returns all customer IDs"
   []
     (j/query db
       ["select CustomerID
        from customers"]
       ))
 
+(select-all-customer-ratings 2442)
+(select-all-customer-ratings 30878)
+
 (defn calculate-intersection
-  "Function which returns number of same ratings of two customers"
+  "Function which returns number of the same ratings of the same movie of two customers"
   [CustomerID1 CustomerID2]
   (let [ratings1 (select-all-customer-ratings CustomerID1)
-        ratings2 (select-all-customer-ratings CustomerID2)
-        number 0]
-    (for [rate1 ratings1]
-      (for [rate2 ratings2]
-        (if (and (= (:movieid rate1) (:movieid rate2)) (= (:rating rate1) (:rating rate2)))
-          (inc number)
-        )
-      )
-    )
-  number)
-)
+        ratings2 (select-all-customer-ratings CustomerID2)]
+    (reduce + (flatten
+      (for [rate1 ratings1]
+        (for [rate2 ratings2]
+          (if (and (= (:movieid rate1) (:movieid rate2)) (= (:rating rate1) (:rating rate2)))
+            1
+            0)))))))
 
 (defn calculate-union
   "Function which returns number of all ratings of two customers"
   [CustomerID1 CustomerID2]
   (let [ratings1 (select-all-customer-ratings CustomerID1)
         ratings2 (select-all-customer-ratings CustomerID2)
-        all (+ count(ratings1) count(ratings2))
-        number (- all (calculate-intersection CustomerID1 CustomerID2))]
-   number)
-  )
+        all (+ (count ratings1) (count ratings2))]
+     (- all (calculate-intersection CustomerID1 CustomerID2))))
+
+
+(calculate-intersection 2442 30878)
+(calculate-union 2442 30878)
 
 (defn jaccard-index
   "Function which returns Jaccard similarity coeficient - value between 0 and 1"
   [CustomerID1 CustomerID2]
-  (let [index (/ (calculate-intersection CustomerID1 CustomerID2) (calculate-union CustomerID1 CustomerID2))]
-    index)
-  )
+  (/ (calculate-intersection CustomerID1 CustomerID2) (calculate-union CustomerID1 CustomerID2)))
+
+(jaccard-index 2442 30878)
 
 (defn similarity
   "Function which returns similarity with all customers"
   [CustomerID]
   (let [all-customers-list (all-customers)]
-    (for [customer all-customers-list]
-      (let [index (jaccard-index CustomerID (:customerid customer))]
-        (assoc {} :customerid (:customerid customer) :jacIndex index)
-      )
-    )
-  ))
+    (for [x all-customers-list :let [y (assoc x :index (jaccard-index CustomerID (:customerid x)))]]
+       y)))
 
-  ;(defn top10-similar-customers
-   ; "Function which returns top 10 similar customers"
-   ; [CustomerID]
-   ; (let [similar (similarity CustomerID)]
-   ;   )
-   ; )
+(similarity 2442)
+
+
+;TEST SIMILARITY
+(def test-list (list {:customerid 30878}
+                    {:customerid 337541}
+                    {:customerid 786312}
+                    {:customerid 93986}
+                    {:customerid 4326}
+                    {:customerid 880166}))
+
+(defn similarity-test
+  [CustomerID]
+    (let [all-customers-list test-list]
+    (for [x all-customers-list :let [y (assoc x :index (jaccard-index CustomerID (:customerid x)))]]
+       y)))
+
+(similarity-test 2442)
+;
+
+(defn most-similar-10customers
+  "Returns 10 most similar customers"
+  [CustomerID]
+  (take 10 (reverse (sort-by :index (similarity CustomerID)))))
+
+(defn recommended-movies
+  "Returns 10 recommended movies"
+  [CustomerID]
+  (let [customer10-list (most-similar-10customers CustomerID)]
+  (j/query db ["select MovieID,Title
+               from movies inner join ratings on movies.MovieID = ratings.MovieID
+               where CustomerID in ("
+               (string/join "," (into [] (for [x customer10-list] (str (:customerid x)))))")
+               and MovieID not in ( select MovieID from ratings where CustomerID = ? )
+               order by Rating desc limit 10" CustomerID])))
+
+
+;TEST RECOMMENDATION
+(defn most-similar-3customers-test
+  "Returns 10 most similar customers"
+  [CustomerID]
+  (take 3 (reverse (sort-by :index (similarity-test CustomerID)))))
+(most-similar-3customers-test 2442)
+
+(defn recommended-movies-test
+  "Returns 3 recommended movies"
+  [CustomerID]
+  (let [customer3-list (most-similar-3customers-test CustomerID)]
+      (j/query db ["select MovieID,Title
+               from movies inner join ratings on movies.MovieID = ratings.MovieID
+               where CustomerID in ("
+               (string/join "," (into []
+                    (for [x customer3-list] (str (:customerid x))))) ") order by Rating desc limit 10"])))
+
+(recommended-movies-test 2442)
+;
+
+
+
